f286be849bd55ccdfa4a21e5aeee409a02156113
Dasha <dasha.ogulia1994@gmail.com>
2021-03-11 17:32:07 +0200

    added new pictures

--- src/App/Main/FirstBlock/cities.js
+++ src/App/Main/FirstBlock/cities.js
@@ -1,39 +1,39 @@
 const cities =[
     {
     id: 1,
-    img:"/src/common/style/img/poznan.png",
+    img:"/img/poznan.png",
     city:"Poznan",
     region:"Wielkopolskie",
     },
     {
     id:2,
-    img:"/img/warshawa 12.45.05.png",
+    img:"/img/warshawa.png",
     city:"Warszawa",
     region:"Mazowieckie",
     },
 
     {
     id:3,
-    img:"/img/wroclaw 12.45.05.png",
+    img:"/img/wroclaw.png",
     city:"Wroclaw",
     region:"Dolnoslaskie",
     },
     {
     id:4,
-    img:"/img/karpach 12.45.05.png",
+    img:"/img/karpach.png",
     city:"Karpacz",
     region:"Dolnoslaskie",
     },
     {
     id:5,
-   img:"/img/dolina 12.45.05.png",
+   img:"/img/dolina.png",
     city:"Dolina Olczyska",
     region:"Malopolskie",
     },
 
     {
     id:6,
-    img:"/img/kozy 12.45.05.png",
+    img:"/img/kozy.png",
     city:"Kozy",
     region:"Slaskie",
     },
